0272f04e82dfbc06fa6066d2adfd69e3b8f8d69d
Gouache-Studio <zoobellmint@khu.ac.kr>
2022-01-26 19:12:54 +0900

    뼈대5

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 
+class BaseModel(models.Model):
+    objects = models.Manager()
 
-class HelloWorld(models.Model):
+    class Meta:
+        abstract = True
+
+
+class HelloWorld(BaseModel):
     text = models.CharField(max_length=255, null=False)
